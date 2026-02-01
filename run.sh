80889b069cbb7efcf53f1befdfdf530b48cbd56f
Aglair <github.panama228@passfwd.com>
2026-02-01 00:56:54 -0400

    fix: resolve link simbólico no lançador run.sh

--- run.sh
+++ run.sh
@@ -1,5 +1,19 @@
 #!/usr/bin/env bash
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-cd "$DIR"
-source .venv/bin/activate
-exec python3 main.py "$@"
+
+source="${BASH_SOURCE[0]}"
+while [ -h "$source" ]; do
+  dir="$( cd -P "$( dirname "$source" )" >/dev/null 2>&1 && pwd )"
+  source="$(readlink "$source")"
+  [[ $source != /* ]] && source="$dir/$source"
+done
+dir="$( cd -P "$( dirname "$source" )" >/dev/null 2>&1 && pwd )"
+
+cd "$dir"
+
+if [ -d ".venv" ]; then
+    source .venv/bin/activate
+    exec python3 main.py "$@"
+else
+    echo "erro: ambiente virtual (.venv) não encontrado em $dir"
+    exit 1
+fi
